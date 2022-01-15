a809ccac5a537528f9e15375fac1b167eea4672f
Coder121307 <78479019+Coder121307@users.noreply.github.com>
2022-01-15 13:00:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function bedroom_page(){
+    window.location = "bedroom.html";
+}
+
+function desk_page(){
+    window.location = "desk.html";
+}
+
+function tv_ac_page(){
+    window.location = "TV.html";
+}
